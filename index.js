6b92a7807bb3216f8770875c195b8ab2e3d2a930
mickeymarse <mmserrapica@tutanota.com>
2024-06-23 15:11:23 +0100

    updated cow

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const format = require('./lib/format');
 const data = require('./lib/data');
 
-module.exports = ({ json, cow = 'unipony-smaller' }) =>
+module.exports = ({ json, cow = 'spidercow' }) =>
   json ? JSON.stringify(data) : format({ data, cow });
